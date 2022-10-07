b196328980812c604d9b848d5f1becf8ce52f94c
BoxingBox <c.tzoulias@gmail.com>
2022-10-07 10:27:30 +0300

    Revert "Separation"
    This reverts commit fdbde882c702e8978c99baba0e784f042e09bdd0.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
-﻿using Bin2Dec;
-
-Console.WriteLine("Please enter a binary number up to 8 digits long.");
+﻿Console.WriteLine("Please enter a binary number up to 8 digits long.");
 Console.WriteLine("-------------------------------------------------");
 
 string input = Console.ReadLine();
 int flag=0;
 int flagtwo=0;
+int intResultTwo = 0;
+int intResult = 0;
 string inputCheck = input.ToString();
 
 for (int i = 0; i < inputCheck.Length; i++)
@@ -22,7 +22,8 @@ if (inputCheck.Length <= 8 && flagtwo==0)
 {
     
     Console.WriteLine(" The binary number entered was: " + input);
-    Console.WriteLine("Decimal result is: " + Calculations.Calculation(inputCheck));
+    Calculation(inputCheck);
+    Console.WriteLine("Decimal result is: " + intResult);
 }  
 else if (inputCheck.Length > 8)
 {
@@ -39,3 +40,18 @@ else
 
 
 
+ int Calculation(string a)
+{
+    for (int i = 0; i < a.Length; i++) //1001
+    {
+
+        string stringResult = a.Substring(i, 1);
+        intResult = Int32.Parse(stringResult);
+        if (i == 0) intResult = intResult + (2 * 0);
+        else intResult = intResult + (2 * intResultTwo);
+        intResultTwo = intResult; //4
+
+
+    }
+    return intResult;
+}
